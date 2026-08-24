51e9788292e88a679e0f9f07987a5504cc4a626f
Hanko <hanko142@gmail.com>
2026-08-24 10:46:19 +0200

    wait for visible webview in e2e

--- smoke/webview-e2e-host.js
+++ smoke/webview-e2e-host.js
@@ -35,6 +35,9 @@ async function run() {
   await vscode.commands.executeCommand('workbench.view.extension.runlist');
   await vscode.commands.executeCommand('runlist.projects.focus');
   await waitFor(() => Boolean(api.provider.view), 'The Runlist webview did not open.');
+  api.provider.view.show(false);
+  await waitFor(() => api.provider.view?.visible === true,
+    'The Runlist webview did not become visible.');
 
   fs.writeFileSync(path.join(root, 'host-ready.json'), JSON.stringify({
     fixturePath,
